71f7a9945390e070ddd3dfa0ce9fc3ac842094c9
omar20261 <mero20261@gmail.com>
2026-03-07 11:38:10 +0200

    Initial Commit

---
+++ QuranMajeed/QuranMajeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranMajeedApp.swift
+//  QuranMajeed
+//
+//  Created by Elsehrawy on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct QuranMajeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
